c7e3a1fae2c874f4af353daf1d6a0e1afef45ecf
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2021-04-05 20:31:31 -0400

    Update GraphicsPub.py

--- GraphicsPub.py
+++ GraphicsPub.py
@@ -1,8 +1,11 @@
 import time
 from selenium import webdriver
 
-##Using the Chromedriver to talk it webpage. Other Explorers may not need this, they might see SeleniumDoc
-browser = webdriver.Chrome('Driver path')
+#This has been tested using chrome, I have not tested it with any other web browser, be sure to consult slenium doc for more. 
+browser = webdriver.Chrome('Driver path') """This is where you'll key in the drivever path where you installed the chromedriver for selenium.
+                                             for information on this driver and download go to https://chromedriver.chromium.org/getting-started
+                                             the path should look like 'C:\\user\\...\\chromdriver.exe'. For mac and unix based users remember it 
+                                             should look like 'drive/user/.../chromedriver.exe'"""
 
 ##Grab best buy webpage
 browser.get("https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440")
@@ -12,14 +15,21 @@ buyButton = False
 
 while not buyButton:
     try:
-        #time.sleep(2)
+        #time.sleep(2)  
+        """each time.sleep call is used to prevent code from loading before the webpage does.
+           if you find that the code is executing before your page can load try increasing the sleep increment by
+           1, I.E. time.sleep(3) instead of time.sleep(2). Remember if # proceeds the statement it will not be read.
+           See python documentation for more."""
+        
         #If this works the button is currently sold out
         addToCartButton = addButton = browser.find_element_by_class_name("btn-disabled")
 
-        #Button Isn't open on the script
-        #print("The Button isn't ready yet")
+        print('not in stock')
+        
 
-        #this refreshes the page with some delay to avoid detection
+        #this refreshes the page 
+        #time.sleep(1)      #Remove the hashtage before running if you're worried about the program getting flagged, shouldn't be a problem though. 
+        
         browser.refresh()
     except:
 
@@ -53,14 +63,19 @@ while buyButton == True:
     AddUsername = browser.find_element_by_id('fld-e')
     AddUsername.click()
 
-    AddUsername.send_keys("email")
+    AddUsername.send_keys("email") #Your email address should be listed here like "JMahoney@jerkmate.com" or whatever
 
     AddPassword = browser.find_element_by_id('fld-p1')
     AddPassword.click()
 
-    AddPassword.send_keys("password")
+    AddPassword.send_keys("password")   #Same thing, your password should look like, "JayzTwoChains33"
+    
+    #These two send keys will automatically fill in your log in info and sign you to checkout. 
 
     SignIn = addButton = browser.find_element_by_class_name('cia-form__controls__submit')
     SignIn.click()
     break
+    #If the program makes it to the end, Be sure to get down to placing your order ASAP, even with this, the odds are still stacked against you.
+    #You just have a better chance now is all. Good luck and happy hunting. No this cannot be used to buy multiple in a drop, I designed it that way.
+    #This code may only be moddified with the explicit consent of the author. 
 
